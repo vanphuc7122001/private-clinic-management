ffc6fd10818f9f102cc85a8d483bf93d50dcff50
Phuc lucas <dangvanphuc7122001@gmail.com>
2023-11-18 01:47:38 +0700

    standard file databaseservice

--- backend/src/services/database.service.ts
+++ backend/src/services/database.service.ts
@@ -16,6 +16,58 @@ class DatabaseService {
       await this.instance.$disconnect()
     }
   }
+
+  get users() {
+    return this.instance.user
+  }
+
+  get refreshTokens() {
+    return this.instance.refreshToken
+  }
+
+  get roles() {
+    return this.instance.role
+  }
+
+  get posts() {
+    return this.instance.post
+  }
+
+  get medicalRecords() {
+    return this.instance.medicalRecord
+  }
+
+  get appointments() {
+    return this.instance.appointment
+  }
+
+  get staffSchedules() {
+    return this.instance.staffSchedules
+  }
+
+  get specializations() {
+    return this.instance.specializations
+  }
+
+  get doctorProfiles() {
+    return this.instance.doctorProfile
+  }
+
+  get bills() {
+    return this.instance.bill
+  }
+
+  get prescriptions() {
+    return this.instance.prescriptions
+  }
+
+  get prescriptionDetails() {
+    return this.instance.prescriptionsDetail
+  }
+
+  get medicines() {
+    return this.instance.medicines
+  }
 }
 
 const databaseService = new DatabaseService()
