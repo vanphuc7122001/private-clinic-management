9668a0eeaad5dd45a3456dd15c4e1ad9cdf5811d
Phuc lucas <dangvanphuc7122001@gmail.com>
2023-11-18 15:04:57 +0700

    finalized error handler

--- backend/src/middlewares/error.middlewares.ts
+++ backend/src/middlewares/error.middlewares.ts
@@ -1,18 +1,31 @@
 import { Request, Response, NextFunction } from 'express'
 import { omit } from 'lodash'
+import HTTP_STATUS from '~/constants/httpStatus'
 import { ErrorWithStatus } from '~/models/Errors'
 
 export const defaultErrorHandler = (err: any, req: Request, res: Response, next: NextFunction) => {
-  if (err instanceof ErrorWithStatus) {
-    return res.status(err.status || 500).json(omit(err, ['status']))
-  }
-  Object.getOwnPropertyNames(err).forEach((key) => {
-    Object.defineProperty(err, key, {
-      enumerable: true
+  try {
+    if (err instanceof ErrorWithStatus) {
+      return res.status(err.status).json(omit(err, ['status']))
+    }
+    const finalError: any = {}
+    Object.getOwnPropertyNames(err).forEach((key) => {
+      if (
+        !Object.getOwnPropertyDescriptor(err, key)?.configurable ||
+        !Object.getOwnPropertyDescriptor(err, key)?.writable
+      ) {
+        return
+      }
+      finalError[key] = err[key]
     })
-  })
-  return res.status(err.status || 500).json({
-    message: err.message,
-    errors: omit(err, ['stack', 'message'])
-  })
+    return res.status(HTTP_STATUS.INTERNAL_SERVER_ERROR).json({
+      message: finalError.message,
+      errors: omit(finalError, ['stack', 'message'])
+    })
+  } catch (error) {
+    return res.status(HTTP_STATUS.INTERNAL_SERVER_ERROR).json({
+      message: 'Internal server error',
+      errors: omit(error as any, ['stack', ',message'])
+    })
+  }
 }
