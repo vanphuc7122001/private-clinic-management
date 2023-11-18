7fb8f2657e9425abd118967827c3e6d81055421e
Phuc lucas <dangvanphuc7122001@gmail.com>
2023-11-18 14:18:12 +0700

    limit the number of call requests with rate limit

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,10 @@
-import express from 'express'
 import { envConfig } from './constants/config'
-import databaseService from './services/database.service'
 import { initRoutes } from './routes'
-import helmet from 'helmet'
 import cors, { CorsOptions } from 'cors'
+import databaseService from './services/database.service'
+import express from 'express'
+import helmet from 'helmet'
+import rateLimit from 'express-rate-limit'
 
 const app = express()
 
@@ -11,10 +12,18 @@ const port = envConfig.port
 const corsOptions: CorsOptions = {
   origin: envConfig.clientUrl
 }
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false // Disable the `X-RateLimit-*` headers
+  // store: ... , // Use an external store for more precise rate limiting
+})
 
 //middlewares
 app.use(helmet())
 app.use(cors(corsOptions))
+app.use(limiter)
 
 //inital routes
 initRoutes('/api/v1', app)
